e958cdc6c9146d0b3827e2fbf4fadde0e3fd1038
Mattia Lucca <mattialucca92@gmail.com>
2025-02-15 12:11:08 +0100

    modifiche

--- dettaglioArticolo.js
+++ dettaglioArticolo.js
@@ -3,12 +3,16 @@ $(document).ready(function () {
   $("#btn-articoli-simili").click(function () {
     let urlParams = new URLSearchParams(window.location.search);
     let categoria = urlParams.get("cat");
-    console.log(categoria);
+    // console.log(categoria);
     $.ajax({
       url: `http://localhost:8080/api/articoli/categoria/${categoria}`, // Endpoint Spring Boot
       method: "GET",
       success: function (data) {
-        console.log(data);
+        // let urlParams = new URLSearchParams(window.location.search);
+        // let id = urlParams.get("id");
+        // $("#" + id).hide();
+        // console.log("nascondoId" + id);
+        generateArticoliSimili(data);
       },
       error: function (error) {
         console.error("Errore durante la richiesta:", error);
@@ -24,7 +28,7 @@ function loadDetails() {
   let urlParams = new URLSearchParams(window.location.search);
   let articoloId = urlParams.get("id");
 
-  console.log(articoloId);
+  // console.log(articoloId);
 
   $.ajax({
     url: `http://localhost:8080/api/articoli/details/${articoloId}`, // Endpoint Spring Boot
@@ -46,7 +50,7 @@ function generateDetails(data) {
 
   $("#dettaglio-articolo").append(
     `
-    <div class="card" style="width: 18rem;">
+    <div id="${data.id}" class="card" style="width: 18rem;">
             <img src="${data.immagineUrl}" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title">${data.nome}</h5>
@@ -57,4 +61,26 @@ function generateDetails(data) {
     </div>
             `
   );
+}
+
+function generateArticoliSimili(data) {
+  let urlParams = new URLSearchParams(window.location.search);
+  let id = urlParams.get("id");
+
+  data.forEach(function (art) {
+    if (art.id !== id) {
+      $("#dettaglio-articolo-simili").append(`
+        <div class="card" style="width: 18rem;">
+              <img src="${art.immagineUrl}" class="card-img-top" alt="...">
+          <div class="card-body">
+              <h5 class="card-title">${art.nome}</h5>
+              <p class="card-text">${art.categoria}</p>
+              <p class="card-text">${art.descrizione}</p>
+              <p class="card-text">${art.prezzo}</p>
+              <a href="./dettaglioArticolo.html?id=${art.id}&cat=${art.categoria}" class="btn btn-primary">Vedi dettaglio</a>        
+          </div>
+      </div>
+        `);
+    }
+  });
 }
